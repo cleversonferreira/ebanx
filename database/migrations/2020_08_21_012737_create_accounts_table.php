5cacc1914403ff2591b023c79c110961afb56275
Cleverson <cleverson_reisferreira@hotmail.com>
2020-08-21 02:45:31 -0300

    add field id on accounts

--- database/migrations/2020_08_21_012737_create_accounts_table.php
+++ database/migrations/2020_08_21_012737_create_accounts_table.php
@@ -14,6 +14,7 @@ class CreateAccountsTable extends Migration
     public function up()
     {
         Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
             $table->integer('account_id')->unique();
             $table->unsignedInteger('user_id')
                 ->foreign('user_id')
