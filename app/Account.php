465380fafefe45d81fa8e2ec8e460e275d994fd3
Cleverson <cleverson_reisferreira@hotmail.com>
2020-08-20 22:38:47 -0300

    create models relationships

--- app/Account.php
+++ app/Account.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
-    //
+    protected $fillable = [
+        //
+    ];
+
+    public function user(){
+        return $this->hasOne(User::class)
+    }
+
+    public function transactions(){
+        return $this->hasMany(Transactions::class)
+    }
 }
--- app/Transactions.php
+++ app/Transactions.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transactions extends Model
 {
-    //
+    protected $fillable = [
+        //
+    ];
+
+    public function account(){
+        return $this->hasOne(Account::class)
+    }
 }
