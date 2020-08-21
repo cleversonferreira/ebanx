1e8126a88c6da16ab2ed0e41687a696431b12695
Cleverson <cleverson_reisferreira@hotmail.com>
2020-08-21 02:58:31 -0300

    create withraw function

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -23,7 +23,7 @@ class TransactionsController extends Controller
                     return $this->deposit($request['destination'], $request['amount']);
                     break;
                 case Types::WITHDRAW :
-                    echo "saque";
+                    return $this->withdraw($request['origin'], $request['amount']);
                     break;
                 case Types::TRANSFER :
                     echo "transferencia";
@@ -108,5 +108,47 @@ class TransactionsController extends Controller
             return $e->getMessage();         
         }
 
+    }
+
+    public function withdraw($origin, $amount){
+        try{
+
+            if(!isset($origin) || empty($origin))
+                throw new \Exception("Origin cannot be null");
+                
+            if(!isset($amount) || empty($amount))
+                throw new \Exception("Amount cannot be null");
+
+            //seach account
+            $account = Account::where('account_id', $origin)->first();            
+            
+            //if account don't exists
+            if(!$account){
+                return response(0, 404);
+            }
+
+            //if account exists, sum amount to current balance
+            $total = ($account->balance - $amount);
+            $account = Account::where('account_id', $origin)->update(array('balance' => $total));
+
+            //create transaction
+            Transactions::create([
+                'account_id' => $origin,
+                'type' => Types::WITHDRAW,
+                'destination' => $origin,
+                'amount' => $amount,
+            ]);
+            
+            return Response::json([
+                'origin' => [
+                    "id" => $origin,
+                    "balance" => $total
+                ]
+            ], 201);
+
+
+        }catch(\Exception $e){
+            return $e->getMessage();         
+        }
     }
 }
